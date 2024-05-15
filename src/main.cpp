2c57a933da4900de781e73ac7eda35302e792a50
Josh Garvin <josh@Joshs-Mac-mini.local>
2024-05-15 12:57:08 -0500

    Add main loop

--- src/main.cpp
+++ src/main.cpp
@@ -8,7 +8,17 @@ const string WELCOME = "\nWelcome to Student Record Keeper v0.1\n\n";
 
 int main(int argc, char** argv) {
     
+    bool terminate = false;
+    
     userWelcome(argc, argv, WELCOME);
+
+    while (!terminate) {
+        displayMainMenu();
+        terminate = true;
+        // get user selection >>
+        // optionIsValid()
+        // call requested function
+    }
 
     return 0;
 }
