1335d78466fa58e764b0fa380dc8e2e27facc864
Josh Garvin <josh@Joshs-Mac-mini.local>
2024-05-20 11:43:41 -0500

    Create Student class

--- include/student.h
+++ include/student.h
@@ -5,6 +5,34 @@
 #ifndef STUDENTS_DATA_H
 #define STUDENTS_DATA_H
 
+#include "course.h"
 #include <iostream>
+#include <string>
+#include <vector>
+using namespace std;
+
+class Student {
+
+    private:
+        string fName;
+        string lName;
+        vector<Course> courses;
+
+    public:
+        // Constructor
+        Student(string, string);
+
+        // Destructor
+        ~Student();
+
+        // Getters
+        string getFirstName();
+        string getLastName();
+
+        // Setters
+        void setFirstName(string);
+        void setLastName(string);
+
+};
 
 #endif // STUDENTS_DATA_H
--- src/student.cpp
+++ src/student.cpp
@@ -3,3 +3,27 @@
 */
 
 #include "../include/student.h"
+
+Student::Student(string firstName, string lastName) {
+    setFirstName(firstName);
+    setLastName(lastName);
+} // end of Student()
+
+Student::~Student() {
+} // end of ~Student()
+
+void Student::setFirstName(string firstName) {
+    fName = firstName;
+} // end of setFirstName()
+
+void Student::setLastName(string lastName) {
+    lName = lastName;
+} // end of setLastName()
+
+string Student::getFirstName() {
+    return fName;
+} // end of getFirstName()
+
+string Student::getLastName() {
+    return lName;
+} // end of getLastName()
