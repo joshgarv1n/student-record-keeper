0d5623d0c042de33e074d4b51f3c19f63a02785c
Josh Garvin <josh@Joshs-Mac-mini.local>
2024-05-24 15:24:24 -0500

    Create menu functionality

--- src/main.cpp
+++ src/main.cpp
@@ -19,17 +19,51 @@ const vector<int> allowedHours = {1, 2, 3, 4};
 int main(int argc, char** argv) {
     
     bool terminate = false;
-
+    int choice;
     Student thisStudent;
-    
+
     userWelcome(WELCOME, thisStudent);
 
     while (!terminate) {
         displayMainMenu();
-        terminate = true;
-        // get user selection >>
-        // optionIsValid()
-        // call requested function
+        cout << "Choose an option: ";
+        cin >> choice;
+
+        if (cin.fail()) {
+            cout << "Invalid input. Please enter an integer." << endl;
+            cin.clear(); // Clear the error flag
+            cin.ignore(numeric_limits<streamsize>::max(), '\n'); // Discard the invalid input
+            continue; 
+        }
+
+        switch (choice) {
+            case 1:
+                addNewCourse(thisStudent, allowedSemesters, allowedGrades, allowedHours);
+                break;
+            case 2:
+                cout << "Edit a course... under construction..." << endl;
+                break;
+            case 3:
+                cout << "Remove a course... under construction..." << endl;
+                break;
+            case 4:
+                cout << "View course(s)... under construction..." << endl;
+                break;
+            case 5:
+                cout << "Calculate my GPA... under construction..." << endl;
+                break;
+            case 6:
+                cout << "Export to CSV... under construction..." << endl;
+                break;
+            case 7:
+                cout << "Exiting program..." << endl;
+                cout << "Goodbye." << endl;
+                terminate = true;
+                break;
+            default:
+                cout << "Invalid choice. Please try again." << endl;
+                break;
+        }
     }
 
     return 0;
