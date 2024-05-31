a57850be5261965d9e8bd9d494fe0022a676fa03
Josh Garvin <josh@Joshs-Mac-mini.local>
2024-05-31 15:23:38 -0500

    Create fs namespace alias, loadCourses(), searchCourseMap(), chooseCourseID(), chooseCourseName(), confirmCourseSelection(), pressEnterToContinue(), toUpperCase(). Remove getIntegerInput().

--- include/functions.h
+++ include/functions.h
@@ -7,6 +7,7 @@
 
 #include "student.h"
 #include "course.h"
+#include "course_map.h"
 #include <iostream>
 #include <iomanip>          // for std::setw
 #include <string>
@@ -14,17 +15,27 @@
 #include <unistd.h>         // for sleep()
 #include <algorithm>        // for std::find
 #include <limits>           // for std::numeric_limits
+#include <filesystem>       
+#include <fstream>
+#include <sstream>
 using namespace std;
+namespace fs = std::filesystem;
 
 // Core functions
 void userWelcome(string, Student&);
 void displayMainMenu(const vector<string>&);
 
+// CourseMap functions
+void loadCourses(CourseMap&);
+string searchCourseMap(const CourseMap&, const string&);
+
 // Menu functions
-void addNewCourse(Student&, const vector<string>&, const vector<string>&, const vector<int>&);
+void addNewCourse(Student&, CourseMap&, const vector<string>&, const vector<string>&, const vector<int>&);
 void removeCourse(Student&);
 void removeCourseMenuSelection(const string&, const vector<string>&, int&);
 void setUserName(string&, string&);
+void chooseCourseID(CourseMap&, string&, string&);
+void chooseCourseName(string&);
 void chooseSemester(string&, const vector<string>&);
 void chooseGrade(string&, const vector<string>&);
 void chooseHours(int&, const vector<int>&);
@@ -34,8 +45,10 @@ bool isYesNo(string, char&);
 void validateUserName(string&, string&, Student&);
 bool isValidInput(const vector<string>&, const string&);
 void getStringInput(string, string&);
-void getIntegerInput(int&);         // ***unused***
 bool confirmIntSelection(const int&);
+bool confirmCourseSelection(const string&, const string&);
 void printVectorMenu(const vector<string>&);
+void pressEnterToContinue();
+void toUpperCase(string&);
 
 #endif // STUDENTS_FUNCTIONS_H
