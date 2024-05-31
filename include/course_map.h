ab9ac190e335d18dfc011c8714369cc6aa76115d
Josh Garvin <josh@Joshs-Mac-mini.local>
2024-05-31 10:19:26 -0500

    Initial commit

---
+++ include/course_map.h
@@ -0,0 +1,11 @@
+#ifndef COURSE_MAP_H
+#define COURSE_MAP_H
+
+#include <unordered_map>
+#include <string>
+
+using namespace std;
+
+using CourseMap = unordered_map<string, unordered_map<string, string>>;
+
+#endif // COURSE_MAP_H
